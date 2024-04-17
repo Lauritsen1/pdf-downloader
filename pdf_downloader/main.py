74717d87176f0ac8b8609506fe75650db6285cf2
Rasmus Lauritsen <43439056+Lauritsen1@users.noreply.github.com>
2024-04-17 14:55:32 +0200

    add PDF downloading functionality

--- pdf_downloader/main.py
+++ pdf_downloader/main.py
@@ -1,5 +1,19 @@
+import shutil
+
+import pandas as pd
+import requests
+
+
 def main():
-    pass
+    columns = ['BRnum', 'Pdf_URL', 'Report Html Address']
+    df = pd.read_csv('data/GRI_2017_2020.csv', usecols=columns)
+
+    url = df['Pdf_URL'][0]
+    local_filename = df['BRnum'][0] + '.pdf'
+
+    with requests.get(url, stream=True) as r:
+        with open(local_filename, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
 
 
 if __name__ == '__main__':
